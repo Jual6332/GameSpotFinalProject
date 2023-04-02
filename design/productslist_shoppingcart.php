4c0d6fe423987e295fc373b2342b534a4aacf841
Justin Alvey <jual6332@colorado.edu>
2023-04-01 20:52:48 -0400

    Parsing of database data to the front-end

--- design/productslist_shoppingcart.php
+++ design/productslist_shoppingcart.php
@@ -7,9 +7,10 @@
         ?>
             <div class="product-item">
                 <form method="post" action="index.php?action=add&sku=<?php echo $product_list[$key]["sku"];?>">
-                <div class="product-image"></div>
-                <div class="product-title"></div>
-                <div class="product-price"></div>
+                <div class="product-image"></div><img src="<?php echo $product_array[$key]["image"]; ?>"></div>
+                <div class="product-tile-footer">
+                <div class="product-title"></div><?php echo $product_array[$key]["name"]; ?></div>
+                <div class="product-price"></div><?php echo "$".$product_array[$key]["price"]; ?></div>
                 <div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2"><input type="submit" value="Add to Cart" class="btnAddAction"></div>
             </div>
         <?php
