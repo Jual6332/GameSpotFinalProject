fd58f2826a7c677eab5259f39199044dedaa5744
Justin Alvey <jual6332@colorado.edu>
2023-04-22 13:59:26 -0400

    Started table for shopping cart items

--- design/productslist_shoppingcart.php
+++ design/productslist_shoppingcart.php
@@ -1,3 +1,35 @@
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
+<tbody>
+    <tr>
+        <th style="text-align:left;">Name</th>
+        <th style="text-align:left;">Code</th>
+        <th style="text-align:right" width="5%"> Quantity</th>
+    </tr>
+
+<?php
+    if(isset($_SESSION["cart_item"])){
+        $total_quantity = 0;
+        $total_price = 0;
+    }
+
+    foreach ($_SESSION["cart_item"] as $item){
+        $cost = $item["quantity"]*$item["price"];
+        ?>
+            <tr>
+                <td><img src="<?php echo $item["image"];?>"/></td> 
+                <td><?php echo $item["code"]; ?></td>
+                <td style="text-align:right;"> <?php echo $item["quantity"]; ?></td>
+                <td style="text-align:right;"> <?php echo $item["price"]; ?></td>
+            </tr>
+            <?php
+            $total_quantity += $item["quantity"];
+            $total_cost += ($item["quantity"]*$item["price"]);
+    }
+?>
+
+</tbody>
+</table>
+
 <div id="product-grid">
     <div class="txt-heading">Products We Offer</div>
     <?php
