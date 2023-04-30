8c1098a27629c8898a0f0d04cf90b0a1a6ac9dca
Justin Alvey <jual6332@colorado.edu>
2023-04-29 20:43:17 -0400

    CreateProductsTable script and insert values

---
+++ design/createProductsTableScript.sql
@@ -0,0 +1,21 @@
+--
+-- Table structure for table `tblproduct`
+--
+
+CREATE TABLE `products` (
+  `id` int(8) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `image` text NOT NULL,
+  `price` double(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `sku`, `image`, `price`) VALUES
+(1, 'FinePix Pro2 3D Camera', '3DcAM01', 'product-images/camera.jpg', 1500.00),
+(2, 'EXP Portable Hard Drive', 'USB02', 'product-images/external-hard-drive.jpg', 800.00),
+(3, 'Luxury Ultra thin Wrist Watch', 'wristWear03', 'product-images/watch.jpg', 300.00),
+(4, 'XP 1155 Intel Core Laptop', 'LPN45', 'product-images/laptop.jpg', 800.00);
