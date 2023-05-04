cd4ce04ece36a9ba22f83ce2d177e6a62f398623
Justin Alvey <jual6332@colorado.edu>
2023-05-04 17:06:01 -0400

    Added heroku deployment ClearDB connection

--- connection.php
+++ connection.php
@@ -1,16 +1,16 @@
 <?php      
     // Connect to localhost when working locally
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "gamespotcompany";
+    //$host = "localhost";  
+    //$user = "root";  
+    //$password = '';  
+    //$db_name = "gamespotcompany";
 
     // Connect to ClearDB when deploying to Heroku
 
-    //$host = "us-cdbr-east-06.cleardb.net";  
-    //$user = "b0697db6c03ee3";  
-    //$password = '7a4f4698';  
-    //$db_name = "heroku_aecc862be4c87f7"; 
+    $host = "us-cdbr-east-06.cleardb.net";  
+    $user = "b63ea21dacfbc3";  
+    $password = '50cfb7f6';  
+    $db_name = "heroku_b5298790e7bf5bb"; 
       
     // Connect using MySQL
     $con = mysqli_connect($host, $user, $password, $db_name);  
