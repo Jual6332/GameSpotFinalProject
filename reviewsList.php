2131200f3556e1e4ed3da6dd9277555c8a33acaa
Justin Alvey <jual6332@colorado.edu>
2023-05-05 12:11:24 -0400

    Updated headers for reviewes table

--- reviewsList.php
+++ reviewsList.php
@@ -35,16 +35,17 @@
 			<div>
 				<h1>TaskHub</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="#"><i class="fas fa-user-circle"></i>Tasks List</a>
+				<a href="#"><i class="fas fa-user-circle"></i>Reviews List</a>
+                <a href="productslist_shoppingcart.php"><i class="fas fa-user-circle">ProductsList/ShoppingCart</i></a>
 				<a href="employee_roster.php"><i class="fas fa-user-circle"></i>Employee Roster</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 	</body>
 	<div class="content">
-        <h2>Tasks List!</h2>
+        <h2>Reviews List!</h2>
         <div class="mt-5 mb-3 clearfix">
-            <h2 class="pull-left">Task Details</h2>
+            <h2 class="pull-left">Review Details</h2>
             <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
         
         <?php
@@ -53,11 +54,11 @@
         echo "<thead>";
         echo "<tr>";
         //echo "<th>#</th>";
-        echo "<th>Name</th>";
-        echo "<th>Taskee</th>";
+        echo "<th>Title</th>";
+        echo "<th>Reviewer's Name</th>";
         echo "<th>Description</th>";
-        echo "<th>Manager</th>";
-        echo "<th>Performance</th>";
+        echo "<th>Review Tyoe</th>";
+        echo "<th>Rating</th>";
         echo "</tr>";
         echo "</thead>";
         echo "<tbody>";
@@ -66,11 +67,11 @@
         ?>
         
         <tr>
+        <?php echo "<td>" . $row["title"] . "</td>"; ?>
         <?php echo "<td>" . $row["name"] . "</td>"; ?>
-        <?php echo "<td>" . $row["taskee"] . "</td>"; ?>
         <td><?php echo $row["description"]; ?></td>
-        <td><?php echo $row["manager"]; ?></td>
-        <td><?php echo $row["performance"]; ?></td>
+        <td><?php echo $row["reviewType"]; ?></td>
+        <td><?php echo $row["rating"]; ?></td>
         </tr>
         <?php
         $i++;
