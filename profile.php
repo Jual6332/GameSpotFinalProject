84167ab0c7e377870e353b9d42b03210f203abd4
Justin Alvey <jual6332@colorado.edu>
2023-05-05 12:34:59 -0400

    Removed unneccessary styling

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@ session_start();
 		<meta charset="utf-8">
 		<title>Welcome!</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+		<!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">-->
 	</head>
 	<?php
 		$_SESSION["cart_item"] = [];
