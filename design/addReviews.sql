f44ae1a6bfd87b2b656fec1b8a3015bc38fa32dd
Justin Alvey <jual6332@colorado.edu>
2023-05-05 12:26:43 -0400

    Add reviews sql script

---
+++ design/addReviews.sql
@@ -0,0 +1,3 @@
+INSERT INTO `reviews` (`title`, `name`, `description`, `reviewType`, `rating`) VALUES
+('Mary is the BEST!', 'Allison Bell','Mary is the best cashier! She was super helpful with purchasing products from your store!','employee', 10.0),
+('The website is easy to use', 'Mark Gold','I wish there were more prodeucts on your website but it is easy to use.','website',7.0);
